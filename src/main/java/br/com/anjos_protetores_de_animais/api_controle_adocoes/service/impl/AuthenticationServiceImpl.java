6be4877faf6e764a91baf268d61406f9ff67ccaa
Igor <igoreduardo20101@gmail.com>
2025-10-12 22:38:15 -0300

    feat: adiciona encriptação de senha ao cadastrar usuário, adiciona lógica de busca de usuário e envio de token ao acessar

--- src/main/java/br/com/anjos_protetores_de_animais/api_controle_adocoes/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/br/com/anjos_protetores_de_animais/api_controle_adocoes/service/impl/AuthenticationServiceImpl.java
@@ -1,13 +1,15 @@
 package br.com.anjos_protetores_de_animais.api_controle_adocoes.service.impl;
 
+import br.com.anjos_protetores_de_animais.api_controle_adocoes.config.security.TokenService;
 import br.com.anjos_protetores_de_animais.api_controle_adocoes.domain.dto.LoginDto;
 import br.com.anjos_protetores_de_animais.api_controle_adocoes.domain.entity.Adopter;
 import br.com.anjos_protetores_de_animais.api_controle_adocoes.domain.payload.LoginPayload;
 import br.com.anjos_protetores_de_animais.api_controle_adocoes.domain.payload.SignUpPayload;
 import br.com.anjos_protetores_de_animais.api_controle_adocoes.repository.AdopterRepository;
 import br.com.anjos_protetores_de_animais.api_controle_adocoes.service.AuthenticationService;
-import lombok.NonNull;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,32 +25,51 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     );
 
     private final AdopterRepository repository;
+    private final TokenService tokenService;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthenticationServiceImpl(final AdopterRepository repository) {
+    public AuthenticationServiceImpl(final AdopterRepository repository,
+                                     final TokenService tokenService,
+                                     final PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.tokenService = tokenService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
-    public ResponseEntity<LoginDto> login(@NonNull final LoginPayload payload) {
+    public ResponseEntity<LoginDto> login(@NotBlank final LoginPayload payload) {
         final String email = payload.getEmail();
-        final String password = payload.getPassword();
+        final String rawPassword = payload.getPassword();
 
-        final Optional<Adopter> possibleUser = this.repository.findOneByEmail(email);
+//        final Optional<Adopter> possibleUser = users.stream()
+//                .filter(user -> user.getEmail().equals(email))
+//                .findFirst();
+
+        //  cenário real usando o repositório
+         final Optional<Adopter> possibleUser = this.repository.findOneByEmail(email);
+
         if (possibleUser.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
 
         final Adopter user = possibleUser.get();
-        if (user.getPassword().equals(password)) {
-            final LoginDto response = new LoginDto(
-                    "token",
-                    6969
-            );
-            return ResponseEntity.ok(response);
+
+        // Validar a senha com o PasswordEncoder
+         if (!passwordEncoder.matches(rawPassword, user.getPassword())) {
+//        if (!user.getPassword().equals(rawPassword)) {
+            return ResponseEntity.badRequest().build();
         }
 
-        return ResponseEntity.badRequest()
-                .build();
+        var token = tokenService.generateToken(user);
+        var tokenExpiration = tokenService.generationExpirationDate();
+
+        final LoginDto response = new LoginDto(
+                token,
+                tokenExpiration,
+                "Bearer"
+        );
+        return ResponseEntity.ok(response);
+
     }
 
     @Override
@@ -61,12 +82,14 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public ResponseEntity<?> signUp(final SignUpPayload payload) {
         final String email = payload.getEmail();
         final String name = payload.getName();
-        final String password = payload.getPassword();
+        final String rawPassword = payload.getPassword();
 
         final Optional<Adopter> possibleUser = this.repository.findOneByEmail(email);
         if (possibleUser.isPresent()) {
             return ResponseEntity.badRequest().build();
         }
+
+        final String password = passwordEncoder.encode(rawPassword);
 
         final Adopter user = new Adopter(
                 name, email, password
